1ffcdeaf701827f476d0f8dd16357d7e68f98565
Rhys Harris <rhys.harris@hotmail.co.uk>
2021-05-31 18:07:41 +0100

    Initial commit

---
+++ src/components/Buttons.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Buttons.css';
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SIZES = ['btn--medium', 'btn--large'];
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize,
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+        ? buttonStyle /* if button HAS a style (aka above if it true), apply the one it has */
+        : STYLES[1]; /* if the button DOES NOT have a style, apply the SECOND one in Styles array */
+    
+        const checkButtonSize = SIZES.includes(buttonStyle)
+        ? buttonSize /* if button HAS a size (aka above if it true), apply the one it has */
+        : SIZES[0]; /* if the button DOES NOT have a size, apply the FIRST one in Styles array */
+
+    return (
+        <Link to='/' className='btn-mobile'>
+            <button
+                className={`btn ${checkButtonSize} ${checkButtonStyle}`}
+                onClick={onClick}
+                type={type}
+            >
+                {children}
+            </button>
+        </Link>
+    )
+};
---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom';
+import { Button } from './Buttons';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click); {/* makes it a toggle. ! makes it opposite of current value */}
+    const closeMobileMenu = () => setClick(false);
+
+    /*const showButton = () => { {/* shows button depending on screen width *}
+        if(window.innerWidth<=960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+        }*/
+    /*useEffect(() => {
+        showButton()
+    }, []); /* makes sure button renders once. Otherwise, if refreshing, hidden button appears*/
+
+
+    /*window.addEventListener('resize', showButton);*/
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <div className='menu-icon' onClick={handleClick}> {/* calls the handleClick method above */}
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> {/* on click, toggle class between these two icons */}
+                    </div>
+                    <Link to='/' className='navbar-logo mobile-only' onClick={closeMobileMenu}>
+                        RH
+                    </Link>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}> {/* if true, make it active and manu class, if fales, make it just menu */}
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}> {/* once clicked, call function to close the mobile menu */}
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/design' className='nav-links' onClick={closeMobileMenu}> {/* once clicked, call function to close the mobile menu */}
+                                Design
+                            </Link>
+                        </li>
+                        <Link to='/' className='navbar-logo desktop-only'>
+                        RH
+                        </Link>
+                        <li className='nav-item'>
+                            <Link to='/coding' className='nav-links' onClick={closeMobileMenu}> {/* once clicked, call function to close the mobile menu */}
+                                Coding
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/cv' className='nav-links' onClick={closeMobileMenu}> {/* once clicked, call function to close the mobile menu */}
+                                CV
+                            </Link>
+                        </li>
+                    </ul>
+                    {/*{button && <Button buttonStyle='btn--outline'>TEST</Button>}*/}
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
