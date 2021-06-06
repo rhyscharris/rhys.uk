7dabeac2b54b846891ee20cfcd45af9be07ded6e
Rhys Harris <rhys.harris@hotmail.co.uk>
2021-06-06 23:15:04 +0100

    Hero half done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import './Hero.css';
 import '../App.css'
+import person from './person.png'
+import leftshapes from './left-shapes.png'
+import rightshapes from './right-shapes.png'
+
+
 
 function Hero () {
     return (
         <div className='hero-container'>
-            <p>Hello</p>
-            <h1>Hi, I'm Rhys</h1>
-            <p>A Systems Engineer by day, Digital Designer and occasional coder by night.</p>
+            <div className='hero-container-inner'>
+                <div className='hero-image image-left'>
+                    <img src={leftshapes} alt='Colourful 3D shapes'></img>
+                </div>
+                <div className='hero-items'>
+                    <p>Hello</p>
+                    <h1>I'm Rhys</h1>
+                    <p>A Systems Engineer by day, Digital Designer and occasional coder by night.</p>
+                </div>
+                <div className='hero-items'>
+                    <img src={person} alt='Picture of person'></img>
+                </div>
+                <div className='hero-image image-right'>
+                    <img src={rightshapes} alt='Colourful 3D shapes'></img>
+                </div>
+            </div>
         </div>
     )
 }
