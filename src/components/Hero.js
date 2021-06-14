b72f0017acc128d9daa1a0f24c9653be164c7c3f
Rhys Harris <rhys.harris@hotmail.co.uk>
2021-06-14 21:07:58 +0100

    Hero ""done""
    fully responsive, all screen sizes work

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,6 +7,8 @@ import rightshapes from './right-shapes.png'
 import leftshapes2x from './left-shapes-2x.png'
 import rightshapes2x from './right-shapes-2x.png'
 
+import bottomshapes from './bottom-shapes.png'
+
 import leftshapesmobile from './left-shapes-mobile.png'
 import rightshapesmobile from './right-shapes-mobile.png'
 import leftshapesmobile2x from './left-shapes-mobile-2x.png'
@@ -19,26 +21,28 @@ function Hero () {
         <div className='hero-container'>
             <div className='hero-container-inner'>
                 <div className='hero-image image-left'>
-                    <img src={leftshapes} alt='Colourful 3D shapes'></img>
+                    <picture>
+                        <source media="(min-width: 75em)" src={leftshapes} srcSet={`${leftshapes} 631w, ${leftshapes2x} 1262w`} />
+                        <source media="(min-width: 45em)" src={leftshapesmobile} srcSet={`${leftshapesmobile} 279w, ${leftshapesmobile2x} 558w`} />
+                        <source sizes="" src={bottomshapes} />
+                        {/*<source sizes="100vw" src="https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616774380/website-assets/press/hands_small.png" srcset="https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616774380/website-assets/press/hands_small_2x.png 375w,https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616774380/website-assets/press/hands_small_2x.png 750w"></source>*/}
+                        <img src={bottomshapes} alt='Colourful 3D shapes'></img>
+                    </picture>
                 </div>
-                <div className='hero-items'>
+                <div className='hero-items hero-text'>
                     <p>Hello</p>
                     <h1>I'm Rhys</h1>
                     <p>A Systems Engineer by day, Digital Designer and occasional coder by night.</p>
                 </div>
-                <div className='hero-items'>
+                <div className='hero-items person-image'>
                     <img src={person} alt='Picture of person'></img>
                 </div>
                 <div className='hero-image image-right'>
                     {/*<img src={rightshapes} alt='Colourful 3D shapes'></img>*/}
                     <picture>
-                        <source media="(min-width: 64em)" src={rightshapes} srcSet={`${rightshapes} 631w, ${rightshapes2x} 1262w`} />
-                        <source media="(min-width: 40em)" src={rightshapesmobile} srcSet={`${rightshapesmobile} 279w, ${rightshapesmobile2x} 558w`} />
+                        <source media="(min-width: 75em)" src={rightshapes} srcSet={`${rightshapes} 631w, ${rightshapes2x} 1262w`} />
+                        <source media="(min-width: 45em)" src={rightshapesmobile} srcSet={`${rightshapesmobile} 279w, ${rightshapesmobile2x} 558w`} />
                         <img src={rightshapes} alt='Colourful 3D shapes'></img>
-
-                        {/*<source media="(min-width: 64em)" src="https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616682131/website-assets/press/hands_right.png" srcset="https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616682131/website-assets/press/hands_right.png 631w,https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616686140/website-assets/press/hands_right_2x.png 1262w"></source>
-                        <source media="(min-width: 40em)"  src="https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616776346/website-assets/press/hands_right_tablet.png" srcset="https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616776346/website-assets/press/hands_right_tablet.png 279w,https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616776346/website-assets/press/hands_right_tablet_2x.png 558w"></source>
-                        <img alt="Press Microphones" src="https://res.cloudinary.com/pitch-software/image/upload/f_auto/v1616776346/website-assets/press/hands_right_tablet.png" title=""></img>*/}
                     </picture>
                 </div>
             </div>
