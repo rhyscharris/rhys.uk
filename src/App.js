b4e0c700093d84cee2f5d4e73073cb9d24562e68
Rhys Harris <rhys.harris@hotmail.co.uk>
2022-01-04 23:55:17 +0000

    Gradient less bright, dark mode draft

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import useLocalStorage from 'use-local-storage'
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home'
 
 
+
+
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches; /* Have they got a theme preference set? */
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light'); /* Keep user preference if they set light before */
   return (
     <>
       <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component = {Home} />
-        </Switch>
+        <div classname="app" data-theme={theme}>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component = {Home} />
+          </Switch>
+          <Footer />
+        </div>
       </Router>
     </>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom';
+/*import './Footer.css';*/
+
+function Footer() {
+    return (
+        <>
+            <p>hi</p>
+        </>
+    )
+}
+
+export default Footer
