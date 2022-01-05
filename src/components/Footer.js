453b724fdca27d0a360bb482fc0609bfc72d1bc8
Rhys Harris <rhys.harris@hotmail.co.uk>
2022-01-05 21:02:08 +0000

    More final footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,24 @@ function Footer({onClick, theme}) {
         <>
             <footer>
                 <div className="footer-wrapper">
-                    <button onClick={onClick}>
+                    <p className="footer-description">Built by myself (Rhys) using React Native, with the code viewable on GitHub. This site is hosted on GCP, and uses the font 'Commuter Sans'. No free cookies or tracking included.</p>
+                    <div class="footer-links">
+                        <p class="name"><a href="/">Rhys Harris</a></p>
+                        <ul>
+                            <li>
+                                <a href="/" class="">Home</a>
+                            </li>
+                            <li>
+                                <a href="/">Design</a>
+                            </li>
+                            <li>
+                                <a href="/">Coding</a>
+                            </li>
+                            <li><a href="/">CV</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <button onClick={onClick} className='theme-button'>
                         Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
                     </button>
                 </div>
