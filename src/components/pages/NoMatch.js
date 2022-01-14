1c293ca2871b6928e22f5e07e9d47d077fa8d860
Rhys Harris <rhys.harris@hotmail.co.uk>
2022-01-14 00:04:49 +0000

    Typewriter on 404 page

--- src/components/pages/NoMatch.js
+++ src/components/pages/NoMatch.js
@@ -9,14 +9,34 @@ import {
     Redirect,
     useLocation
   } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 function NoMatch () {
     let location = useLocation();
     return (
         <div className='full-group'>
             <div className='center-group'>
                 <p>404 - Not Found</p>
-                <h1>No foundo the pageo</h1> 
+                <h1>
+                    <Typewriter
+                        options={{
+                        loop: true
+                        }}
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("No foundo the pageo").start()
+                                .pauseFor(1500)
+                                .deleteAll()
+                                .typeString("Nothing to see here").start()
+                                .pauseFor(1500)
+                                .deleteAll()
+                                .typeString("Sorry, not found").start()
+                                .pauseFor(1500)
+                                .deleteAll()
+                        }}
+                    />
+                </h1> 
                 <h2>Could not find 'rhys.uk{location.pathname}', maybe it's been deleted or never existed</h2>
+                <Link to="/"><h3>Let's go home 🏠</h3></Link>
             </div>
         </div>
     )
